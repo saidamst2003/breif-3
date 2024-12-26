6bceecea2e8f02247b5001fe7732f35dd0f13a39
saidamst2003 <saida2003mst@gmail.com>
2024-12-26 10:55:57 +0100

    j'ajout menu et switch pour classe compte

--- src/Compte.java
+++ src/Compte.java
@@ -34,7 +34,34 @@ public abstract class Compte {
     public void setClient(Client client) {
         this.client = client;
     }
+public void GestionCompte() {
+    Scanner scanner = new Scanner(System.in);
+    int choix;
 
+    do {
+
+        System.out.println("\n--- MENU GESTION DES COMPTES BANCAIRES ---");
+        System.out.println("1. Ajouter un compte courant");
+        System.out.println("2. Ajouter un compte epargne");
+        System.out.println("0. Quitter");
+        System.out.print("Entrez votre choix : ");
+        choix = scanner.nextInt();
+        switch (choix){
+            case 1:
+                AjoutercompteCourant;
+                break;
+
+            case 2:
+                AjouterCompteEpargne;
+                    break;
+            case 0:
+                System.out.println("retoure au menu principal.");
+                break;
+            default:
+                System.out.println("choix invalide.");
+        }
+    }
+}
     @Override
     public String toString() {
         return "Compte{" +
