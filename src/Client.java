c0bfe817c5313cf875eb3ebe6c455a65f7aa854e
saidamst2003 <saida2003mst@gmail.com>
2024-12-26 09:48:03 +0100

    creation de la fonction ajouter et afficher un Client

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,5 @@
+import  java.util.Scanner;
+import java.util.ArrayList;
 
 public class Client {
    private int Id;
@@ -6,15 +8,16 @@ public class Client {
    private String email;
    private String adresse;
    private String telephone;
+   static ArrayList<Client> clients = new ArrayList<>();
 
-public void Client(int Id, String nom, String prenom, String email, String adresse, String telephone){
-this.Id = Id;
-this.nom = nom;
-this.prenom =prenom;
-this.email =email;
-this.adresse =adresse;
-this.telephone =telephone;
-}
+   public  Client(int Id, String nom, String prenom, String email, String adresse, String telephone) {
+      this.Id = Id;
+      this.nom = nom;
+      this.prenom = prenom;
+      this.email = email;
+      this.adresse = adresse;
+      this.telephone = telephone;
+   }
 
    public int getId() {
       return Id;
@@ -44,7 +47,6 @@ this.telephone =telephone;
       return email;
 
 
-
    }
 
    public void setEmail(String email) {
@@ -67,7 +69,51 @@ this.telephone =telephone;
    public void setTelephone(String telephone) {
       this.telephone = telephone;
    }
+public void gestionClient(){
+      Scanner sc = new Scanner(System.in);
+      int choix = -1;
+      while(choix !=0){
+         System.out.println(" --------GESTION DES CLIENTS-----");
+         System.out.println("1. Ajouter un Client : ");
+         System.out.println("2. afficher tous les Client");
+         System.out.println("0. retour au menu Principal");
+         System.out.println();
 
+
+
+
+
+      }
+
+
+}
+   public static void ajouterClient() {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("------- ajouter un nouvel Client-------");
+      System.out.println("ID: ");
+      int id = scanner.nextInt();
+      System.out.println("nom: ");
+      String nom = scanner.next();
+      System.out.println("prenom: ");
+      String prenom = scanner.next();
+      System.out.println("email: ");
+      String email = scanner.next();
+      System.out.println("adresse: ");
+      String adresse = scanner.next();
+      System.out.println("telephone: ");
+      String telephone = scanner.next();
+      System.out.println("Client ajouté : " + nom + " " + prenom);
+       clients.add(new Client(id, nom, prenom, email, adresse, telephone));
+   }
+
+   private void afficherClient() {
+      System.out.println("le nom du Client est : " +getNom());
+      System.out.println("le prenom du Clien est : "+ getPrenom());
+      System.out.println("le email du Client est : "+ getEmail());
+      System.out.println("l'adresse du Client est : "+ getAdresse());
+      System.out.println("le telephone du Client est : "+ getTelephone());
+
+   }
 
    @Override
    public String toString() {
