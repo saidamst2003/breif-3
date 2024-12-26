ef3c78ec23449c91e3c4a626e8b46507fcf90452
saidamst2003 <saida2003mst@gmail.com>
2024-12-26 12:01:13 +0100

    creation de classe CompteCourant et sa menu

--- src/CompteCourant.java
+++ src/CompteCourant.java
@@ -1,2 +1,59 @@
-public class CompteCourant {
+import java.util.Scanner;
+
+public class CompteCourant extends Compte {
+   private double fraisBancaires;
+
+    public CompteCourant(int numero, double solde, Client client, double fraisBancaires) {
+        super(numero, solde, client);
+        this.fraisBancaires = fraisBancaires;
+    }
+
+    public double getFraisBancaires() {
+        return fraisBancaires;
+    }
+
+    public void setFraisBancaires(double fraisBancaires) {
+        this.fraisBancaires = fraisBancaires;
+    }
+
+
+
+    public void GestionCompteCourant(){
+        Scanner scanner = new Scanner(System.in);
+        int choix = scanner.nextInt();
+        while (choix !=0){
+            System.out.println("------Gestion de Compte courant------");
+            System.out.println("1:ajouter un compte courant : ");
+            System.out.println("2:afficher compte courant: ");
+            System.out.println("0.Quitter");
+            System.out.println(" entrer votre choix: ");
+            switch (choix){
+                case 1:
+                    AjoutercompteCourant;
+                    break;
+                case  2:
+                    AfficherComteCourant;
+                    break;
+                case 0:
+                    System.out.println(" retour au menu principal. ");
+                    break;
+                default:
+                    System.out.println("choix invalide.");
+            }
+        }
+    }
+        @Override
+        public String toString() {
+            return "CompteCourant{" +
+                    "fraisBancaires=" + fraisBancaires +
+                    ", client=" + client +
+                    '}';
+
+    }
 }
+
+
+
+
+
+
